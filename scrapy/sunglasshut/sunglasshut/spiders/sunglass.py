b8073b14b93279f77cf660460fe3dec29972d16f
rifat977 <itscrifat5147@gmail.com>
2022-08-05 11:38:38 +0600

    null commit

---
+++ scrapy/sunglasshut/sunglasshut/spiders/sunglass.py
@@ -0,0 +1,15 @@
+import scrapy
+import json
+
+class SunglassSpider(scrapy.Spider):
+    name = 'sunglass'
+    allowed_domains = ['sunglasshut.com']
+    start_urls = ['https://www.sunglasshut.com/wcs/resources/plp/10152/byCategoryId/3074457345626651837?isProductNeeded=true&isChanelCategory=false&pageSize=18&responseFormat=json&currency=USD&catalogId=20602&top=Y&beginIndex=0&viewTaskName=CategoryDisplayView&storeId=10152&langId=-1&categoryId=3074457345626651837&pageView=image&orderBy=default&currentPage=1']
+
+    def parse(self, response):
+        data = json.loads(response.body)
+        yield from data['plpView']['products']['products']['product']
+        next_page = data['plpView']['nextPageURL']
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
